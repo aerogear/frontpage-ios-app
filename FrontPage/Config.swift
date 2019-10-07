49cb12ad853d972595f846a1329a49ecf42d5f07
MStokluska <mstoklus@redhat.com>
2019-10-07 09:44:53 +0100

    styling: indenting

--- FrontPage/Config.swift
+++ FrontPage/Config.swift
@@ -2,17 +2,17 @@ import Foundation
 
 public class Config{
   let config: ServiceConfig
-
-
-public init() {
-  config = ServiceConfig()
+  
+  
+  public init() {
+    config = ServiceConfig()
   }
-
-public func getConfiguration(_ serviceType: String) -> MobileService {
-  let configuration = config.getConfigurationByType(serviceType)
-  if configuration.count > 1 {
-    print("Config contains more than one service of same type")
+  
+  public func getConfiguration(_ serviceType: String) -> MobileService {
+    let configuration = config.getConfigurationByType(serviceType)
+    if configuration.count > 1 {
+      print("Config contains more than one service of same type")
+    }
+    return configuration[0]
   }
-  return configuration[0]
-}
 }
